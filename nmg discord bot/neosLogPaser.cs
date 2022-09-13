b1a8da8a04c2e4c22ec4a91fb095e17adaad556a
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-09-13 17:01:59 -0500

    regex brainstorming

--- nmg discord bot/neosLogPaser.cs
+++ nmg discord bot/neosLogPaser.cs
@@ -1,6 +1,30 @@
+using System.Text.RegularExpressions;
+
 namespace nmgBot
 {
-	internal class neosLogPaser
+	public class NeosLogPaser
 	{
+		// The idea being if a Regex matches a line we can perform certain operations on a it. I love brainstorming.
+
+		// Regex produced by https://regex-generator.olafneumann.org
+		// - Regex can be slow
+		// - Will other locales/languages throw this off?
+
+		// We might not need Regex for the following, IE some of these are hard-coded into every log:
+		// - Presence: (Argument: Libraries\NeosModLoader.dll)
+		// - Version: ([INFO] [NeosModLoader] NeosModLoader {v1.11.0} starting up!)
+		// - Exceptions
+		// - Loaded mods: ([INFO] [NeosModLoader] loaded mod...)
+		// --- Made it anyways kek: new Regex(@"\[(INFO)\].*\[(NeosModLoader)\].*\[.*\].*\(.*\.(dll)+\)", RegexOptions.IgnoreCase);
+
+		// This might be the only actual good use case, identifying if a log is "valid" based on its name.
+		// Ex DESKTOP-4GOD64O - 2022.1.28.1335 - 2022-09-12 20_41_36.log
+		// Look for [2022.1.28.1335, 2022-09-12, 20_41_36.log]
+		private static readonly Regex validLogName = new Regex(@"([0-9]+(\.[0-9]+)+) - [0-9]{4}-[0-9]{2}-[0-9]{2} ([0-9]+(_[0-9]+)+)\.[a-zA-Z]+", RegexOptions.IgnoreCase);
+		
+		public NeosLogPaser()
+		{
+
+		}
 	}
 }
